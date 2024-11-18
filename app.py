a12c02f20026b7cc719135af223c5e0691cde7f9
IanNjeru17 <iannjeru17@gmail.com>
2024-11-18 11:04:53 +0300

    server

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def initialize_database():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app()
+    port = int(os.environ.get("PORT", 5555))
+    app.run(host="0.0.0.0", port=port, debug=True)
