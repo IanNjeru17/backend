982e5fedb2a295e1b18e858511fa23e31cce7833
IanNjeru17 <iannjeru17@gmail.com>
2024-11-18 11:31:18 +0300

    changes

--- app/app.py
+++ app/app.py
@@ -1,13 +1,8 @@
-from app import create_app, db
+import os
+from app import create_app
 
 app = create_app()
 
-# Create database tables before the first request
-@app.before_first_request
-def initialize_database():
-    db.create_all()
-
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get("PORT", 5555))
     app.run(host="0.0.0.0", port=port, debug=True)
